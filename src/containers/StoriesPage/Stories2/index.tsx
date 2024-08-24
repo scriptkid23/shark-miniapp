cd7d9205df52e8880a7c526af71d9574554568ee
Thang Pham <thangpham@PDThangs-Laptop.local>
2024-08-25 01:00:12 +0700

    commit

--- src/containers/StoriesPage/Stories2/index.tsx
+++ src/containers/StoriesPage/Stories2/index.tsx
@@ -17,7 +17,7 @@ const Stories_2: Renderer = ({ story, action }) => {
       <div className="h-full w-full pt-10 px-[10px] text-center">
         <h1 className="text-[44px] font-syneTactile">You are amazing!</h1>
         <p className="font-medium text-[#A7A7A8] text-lg">Here is your BAIT rewards</p>
-        <div className="w-[120px] h-[120px] m-auto mt-[48px]">
+        <div className="w-[125px] h-[193px] m-auto mt-[48px]">
           <img className="w-full h-full" src={HookIcon} alt="animal" />
         </div>
         <h3 className="font-rubik text-5xl mt-[64px]">{numberWithCommas(transaction.point || 0)}</h3>
