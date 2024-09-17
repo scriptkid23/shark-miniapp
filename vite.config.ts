82ddbffb886e0a49e59c9cb87a47690b3910156b
hoando <hoando.dev@gmail.com>
2024-09-17 11:40:01 +0700

    docs: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,12 @@ import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfil
 import mkcert from "vite-plugin-mkcert";
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   preview: {
     host: "0.0.0.0", // Bind to all network interfaces for preview mode as well
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
   },
+
   publicDir: "./public",
   resolve: {
     alias: {
