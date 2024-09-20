04dc172fd148cafa7a119c8f81c98d7888aa821e
hoando <hoando.dev@gmail.com>
2024-09-20 11:36:59 +0700

    click
    click

--- src/containers/HomePage/GameContainer/index.tsx
+++ src/containers/HomePage/GameContainer/index.tsx
@@ -1,9 +1,19 @@
-import GameBannerImage from "@/assets/images/game-banner.png";
-import VaultIcon from "@/assets/icons/vault.png";
-import { useNavigate } from "react-router-dom";
+import GameBannerImage from '@/assets/images/game-banner.png';
+import VaultIcon from '@/assets/icons/vault.png';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+const MIN_CLICK_TIME = 10;
 const GameContainer = () => {
   const navigate = useNavigate();
+  const [numberClicked, setNumberClicked] = useState(0);
 
+  const handleOpenGame = () => {
+    setNumberClicked(numberClicked + 1);
+    if (numberClicked < MIN_CLICK_TIME) {
+      return;
+    }
+    navigate('/shark-game', { state: { from: '/shark-game' } });
+  };
   return (
     <div>
       <h2 className="text-lg font-semibold mb-2 mt-6 flex items-center">
@@ -14,21 +24,14 @@ const GameContainer = () => {
         <img src={GameBannerImage} alt="game" className="w-full h-full" />
         <div className="absolute bottom-0 left-0 right-0 w-full">
           <div className="text-white text-center text-xs font-medium font-rubik flex items-end justify-between pb-4 px-4">
-            <p
-              onClick={() =>
-                navigate("/shark-game", { state: { from: "/shark-game" } })
-              }
-            >
-              Earn infinite $TON with Shark
-            </p>
+            <p onClick={handleOpenGame}>Earn infinite $TON with Shark</p>
 
             <button
               className="flex justify-center items-center px-3 py-2 rounded-lg bg-[#245BD3]"
-<<<<<<< HEAD
-              onClick={() => navigate("/shark-game", { state: { from: "/shark-game" } })}
-=======
+              onClick={() => {
+                console.log('coming soon');
+              }}
               // onClick={() => navigate("/shark-game", { state: { from: "/shark-game" } })}
->>>>>>> b5256eae82e847ce392efa63394972609d7ebf11
             >
               Coming Soon
             </button>
